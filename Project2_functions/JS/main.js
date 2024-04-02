31813a44b84c6ae9aaee03ffeed56f0c65b35fa3
Mason Strewons <159481346+Tehkilla777@users.noreply.github.com>
2024-04-02 13:18:51 -0700

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var words = "Hello!"; //The variable
+    words += " How are you?"; //+= operator adds to the variable
+    document.getElementById("Concatenate").innerHTML = words; //uses document.getElementById()
+}
