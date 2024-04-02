9a7339d686eb776fbb7d3f31f7d1fca3bb95e148
Mason Strewons <159481346+Tehkilla777@users.noreply.github.com>
2024-04-02 11:02:24 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+//Creating variables
+var Sent1 = "This is the beggining of the string"; 
+    Sent2 = " and this is the end of the string";
+
+//Window.alert method
+window.alert("Hello, World!");
+
+//Document.write method displaying the two variables
+document.write(Sent1 + Sent2);
+
+//Document.write displaying an expression
+document.write(12/4);
